2c1eaff0ba06cf4151d3b029f0144129293f3cbd
Nico <nicovigil1@gmail.com>
2019-02-02 15:48:05 -0700

    adds save_user method to create a user in db with github id/info

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,22 @@
 class SessionsController < ApplicationController
     def create
+        response = gh_request_response
+        parse(response)
+        @token = @response_hash["access_token"]
+        oauth_response = Faraday.get("https://api.github.com/user?access_token=#{@token}")
+        auth_info = JSON.parse(oauth_response.body)
+        save_user(auth_info)
+    end
+
+    private
+
+    def gh_request_response
         client_id = "88d1a08a1bda6a44edfc"
         client_secret = ENV["client_secret"]
         code = params[:code]
         response = 
         Faraday.post("https://github.com/login/oauth/access_token?client_id=#{client_id}&client_secret=#{client_secret}&code=#{code}")
-        parse(response)
-        token = @response_hash["access_token"]
-        oauth_response = Faraday.get("https://api.github.com/user?access_token=#{token}")
-        auth_info = JSON.parse(oauth_response.body)
     end
-
-    private
 
     def parse(response)
         pairs = response.body.split("&")
@@ -21,4 +26,13 @@ class SessionsController < ApplicationController
             @response_hash[key] = value
         end
     end 
+
+    def save_user(user_info)
+        user = User.find_or_create_by(uid: user_info["id"])
+        user.username = user_info["login"]
+        user.uid = user_info["id"]
+        user.token = @token
+        user.save
+        session[:user_id] = user.id 
+    end
 end
