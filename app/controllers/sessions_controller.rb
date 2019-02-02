7cf106318f0d6b7435ae5a811599a379e9b5f29b
Nico <nicovigil1@gmail.com>
2019-02-02 15:20:56 -0700

    adds link to login to git hub as well as completes gh user auth in sessions#create

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+    def create
+        client_id = "88d1a08a1bda6a44edfc"
+        client_secret = "d4c70f60f24a3c2581c756ca10e33c4e20675b86"
+        code = params[:code]
+        response = 
+        Faraday.post("https://github.com/login/oauth/access_token?client_id=#{client_id}&client_secret=#{client_secret}&code=#{code}")
+        parse(response)
+        token = @response_hash["access_token"]
+        oauth_response = Faraday.get("https://api.github.com/user?access_token=#{token}")
+        auth_info = JSON.parse(oauth_response.body)
+    end
+
+    private
+
+    def parse(response)
+        pairs = response.body.split("&")
+        @response_hash = {}
+        pairs.each do |pair|
+            key, value = pair.split("=")
+            @response_hash[key] = value
+        end
+    end 
+end
